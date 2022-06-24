2b758d556bcb78485e36d1029c914772b09e3f82
Fgvs-PC <fgvizcainos@gmail.com>
2022-06-24 11:57:33 -0500

    Inicializacion del repo

---
+++ src/scope/global.js
@@ -0,0 +1,15 @@
+var a; //Declarando la variable
+var a = 'aa'; //redeclaracion
+
+var b = 'b'; //Asignacion
+b = 'bb'; //Reasignacion
+
+// Global Scope
+
+var fruit = 'Apple'; //global
+
+function bestFruit(){
+    console.log(fruit);
+}
+
+bestFruit();
